e1fdae52f4dd4105784ce43581afb278b1699ad2
caio avila <caio.avilasp@gmail.com>
2024-04-10 09:08:02 -0300

    Primeiro commit

---
+++ Funcionario.cs
@@ -0,0 +1,33 @@
+﻿class Funcionario
+{
+    private List<Funcionario> funcionarios = new List<Funcionario>();
+    public int Id { get; }
+
+    public string Nome { get;  }
+
+    public double Salario { get; set; }
+
+    public string Cargo { get; }   
+    
+    public Funcionario(string nome, string cargo)
+    {
+        nome = Nome;
+        cargo = Cargo;
+    }
+
+    public void AdicionarFuncionario(Funcionario funcionario)
+    {
+        funcionarios.Add(funcionario);
+    }
+
+    public void ExibirFuncionario()
+    {
+        Console.WriteLine($"O funcionário {Nome}\n:");
+        foreach (Funcionario funcionario in this.funcionarios)
+        {
+            Console.WriteLine($"É {funcionario.Cargo} e recebe um salário de {funcionario.Salario}");
+        }
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Funcionario funcionario1 = new Funcionario("Caio", "Vendedor");
+
+funcionario1.Salario = 1.000;
+funcionario1.AdicionarFuncionario(funcionario1);
+funcionario1.ExibirFuncionario();
+
